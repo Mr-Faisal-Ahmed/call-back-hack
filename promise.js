6e40cff7b994f1f325ec142e973fd2fefde35bf3
Faisal-Ahmed-Khan <51905252+Mr-Faisal-Ahmed@users.noreply.github.com>
2023-11-10 11:34:41 +0500

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,39 @@
+//function hello(){
+    //console.log("hello from 1")
+    //console.log("hello")
+
+//}
+ //function demo(){
+    //console.log("hello from 2")
+    //hello()
+ //}
+//console.log("3rd tab");
+//demo();
+//console.log("bye","nexttime");
+//function one(){
+    //return 4;
+
+//}
+//function two(){
+    //return one() + one() 
+//}
+ 
+//function three(){
+  //  var ans =  one() + two()
+    //console.log(ans);
+//}
+//three();
+//setTimeout(() =>{
+    //console.log("hello world")
+//},3000);
+//setTimeout(()=>{
+    //console.log("what  are you doing")
+//},3000);
+//console.log("welcome");
+h1= document.querySelector("h1");
+
+h1.style.colour = "red";
+setTimeout(()=>{
+    h1.style.colour = "red"
+    
+},2000);
